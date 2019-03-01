c3d3c330ea8b061b07c4d14f618ce401c86b4938
Melissa Hummelt <mkhummelt@gmail.com>
2019-03-01 22:39:42 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,5 +1,6 @@
 class Author
   attr_accessor :name
+  post_count = 0
   def initialize(name)
     @name = name
     @posts = []
@@ -7,9 +8,13 @@ class Author
   def add_post(post)
     @posts << post
     post.author = self
+    post_count += 1
   end
   def add_post_by_title(title)
-    self.create.
+    post = Post.new(title)
+    @posts << post
+    post.author = self
+    post_count += 1
   end
 
 end
