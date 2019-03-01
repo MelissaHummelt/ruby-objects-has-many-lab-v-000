f5e0c8f3c0f77a53515b53a989f68f1fc4ce6f56
Melissa Hummelt <mkhummelt@gmail.com>
2019-03-01 22:43:47 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -5,11 +5,13 @@ class Author
     @name = name
     @posts = []
   end
+  
   def add_post(post)
     @posts << post
     post.author = self
     post_count += 1
   end
+
   def add_post_by_title(title)
     post = Post.new(title)
     @posts << post
